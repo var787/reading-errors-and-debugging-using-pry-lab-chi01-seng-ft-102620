90cf296ae313860665d39805c293ce47d3bc4abd
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-01 07:37:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,9 @@
 require 'pry'
 
 def start_game(player1, player2)
-  binding.pry
-puts  "Hello #{player1} & #{player2}"
+ "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
-  binding.pry
   "#{player1} is better than #{player2}"
 end
